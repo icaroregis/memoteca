e20bf5c79a29c00ad8c251b2bfd5546b088838c2
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-12-02 15:26:04 -0300

    start project
    start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CreateThoughtsComponent } from './components/thoughts/create-thoughts/create-thoughts.component';
+import { DeleteThoughtsComponent } from './components/thoughts/delete-thoughts/delete-thoughts.component';
+import { EditThoughtsComponent } from './components/thoughts/edit-thoughts/edit-thoughts.component';
+import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-thoughts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CreateThoughtsComponent,
+    DeleteThoughtsComponent,
+    EditThoughtsComponent,
+    ListThoughtsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/thoughts/edit-thoughts/edit-thoughts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-thoughts',
+  templateUrl: './edit-thoughts.component.html',
+  styleUrls: ['./edit-thoughts.component.css']
+})
+export class EditThoughtsComponent {
+
+}
