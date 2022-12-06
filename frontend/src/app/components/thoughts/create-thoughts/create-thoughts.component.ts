c0efa6ed033a6466a56439d91709b324f3a8b009
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-12-06 12:15:59 -0300

    tratamento de dados

--- frontend/src/app/components/thoughts/create-thoughts/create-thoughts.component.ts
+++ frontend/src/app/components/thoughts/create-thoughts/create-thoughts.component.ts
@@ -6,35 +6,44 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-thoughts',
   templateUrl: './create-thoughts.component.html',
-  styleUrls: ['./create-thoughts.component.css']
+  styleUrls: ['./create-thoughts.component.css'],
 })
 export class CreateThoughtsComponent {
-  constructor(private fb: FormBuilder, private router: Router, private service: ThoughtService){}
-  ngOnInit(): void { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private service: ThoughtService
+  ) {}
+  ngOnInit(): void {}
 
   public fg: FormGroup = this.fb.group({
     pensamento: [null, Validators.required],
     autoria: [null, Validators.required],
-    cardType: ["modelo1"]
-  })
+    cardType: ['modelo1'],
+  });
 
   createThought(e: any) {
     e.preventDefault();
 
-    const objeto: Thought = {
-      conteudo: this.fg.get("pensamento")?.value,
-      autoria: this.fg.get("autoria")?.value,
-      modelo: this.fg.get("cardType")?.value
+    if (this.fg.invalid) {
+      alert('Por gentileza, é necessário preencher todos os campos!');
+      return;
     }
 
-    if(objeto) {
+    const objeto: Thought = {
+      conteudo: this.fg.get('pensamento')?.value,
+      autoria: this.fg.get('autoria')?.value,
+      modelo: this.fg.get('cardType')?.value,
+    };
+
+    if (objeto) {
       this.service.createThought(objeto).subscribe(() => {
-        this.router.navigate(["/list-thought"]);
-      })
+        this.router.navigate(['/list-thought']);
+      });
     }
   }
 
   cancelThought() {
-    this.router.navigate(["/list-thought"]);
+    this.router.navigate(['/list-thought']);
   }
 }
